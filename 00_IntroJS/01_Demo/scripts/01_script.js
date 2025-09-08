7f32b0204f2adfd3024a2f0d504e1168d7c36cd5
Elena Mastrandrea <elenamastrandrea@Laptop-von-Elena.local>
2025-09-08 12:02:25 +0200

    add demo

---
+++ 00_IntroJS/01_Demo/scripts/01_script.js
@@ -0,0 +1,13 @@
+// Else-If_kette (verschachtelt)
+
+let punktzahl = 35;
+
+if (punktzahl >= 90) {
+    console.log("Sehr gut. Erreichte Punktzahl:"+ punktzahl);
+} else if (punktzahl >= 75) {
+    console.log("Gut. Erreichte Punktzahl:"+ punktzahl);
+} else if (punktzahl >= 50) {
+    console.log("Genügend. Erreichte Punktzahl:"+ punktzahl);
+} else {
+    console.log("Ungenügend. Erreichte Punktzahl:"+ punktzahl);
+}
