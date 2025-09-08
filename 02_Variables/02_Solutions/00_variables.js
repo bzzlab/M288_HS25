8d4df06175c45e902299da971fb73c130e9fec48
Elena Mastrandrea <elenamastrandrea@Laptop-von-Elena.local>
2025-09-08 09:10:43 +0200

    add solutions

--- 02_Variables/02_Solutions/00_variables.js
+++ 02_Variables/02_Solutions/00_variables.js
@@ -1,20 +1,64 @@
 /*
 Übung: LU02.A02 - Variablen und Deklarationen
-
-Wichtig, wenn Werte einer Variable zugewiesen werden:
-Das '='-Zeichen ist ein Zuweisung-Operator und
-nicht ein mathematisches Gleichheitszeichen!!!
  */
 
-// Variable deklarieren
-let zahl1;
-let zahl2;
+/*
+Übung 1: Deklarieren Sie Variablen für folgende Informationen:
+    Ihren Namen (als Text, String)
+    Ihr Alter (als Number)
+    Ihre Lieblingsfarbe (als String)
+Überlegen Sie sich: Verwenden Sie let oder const?
+*/
 
-// Variable initialisieren
-zahl1 = 30;
-zahl2 = 15;
+const name1 = "Anna"; 
+let alter1 = 18; 
+const lieblingsfarbe1 = "Blau";
 
-// Variablen subtrahieren und Wert ausgeben
-console.log(zahl1 - zahl2);
+const name2 = "Max";
+let alter2 = 19;
+const lieblingsfarbe2 = "Gelb";
 
-// Gibt den Wert 15 aus
+
+console.log("Name1:", name1, "Name2:" name2);
+console.log("Alter1:", alter1);
+console.log("Lieblingsfarbe1:", lieblingsfarbe1);
+
+
+/*
+Übung 2:  Rechnen mit Variablen
+
+Deklarieren Sie zwei Variablen x und y mit Zahlenwerten. 
+Berechnen Sie mit diesen Variablen die Summe, Differenz, das Produkt und den Quotienten.
+Geben Sie diese Werte in der Konsole aus.
+*/
+
+// Variablen deklarieren und initialisieren
+let x = 12;
+let y = 4;
+
+let summe = x + y;
+let differenz = x - y;
+let produkt = x * y;
+let quotient = x / y;
+
+
+console.log("Summe:", summe);
+console.log("Differenz:", differenz);
+console.log("Produkt:", produkt);
+console.log("Quotient:", quotient);
+
+/*
+Übung 3:
+*/
+
+{
+    var a = 1;          // mit var deklariert → auch ausserhalb sichtbar
+    let b = 2;          // mit let deklariert → nur im Block sichtbar
+    const c = 3;        // mit const deklariert → nur im Block sichtbar
+
+    console.log(a,b,c);
+}
+
+console.log("a ausserhalb vom Block:",a);
+console.log("b ausserhalb vom Block:",b);
+console.log("c ausserhalb vom Block:",c);
